c8c249622ca7d61fcaa5e7b9273df5ba11d2744d
HEYKASHIF <kashifmohd269@gmail.com>
2023-03-20 02:30:27 +0530

    third commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,152 +1,6 @@
-// import 'dart:ffi';
+import 'dart:ffi';
 
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
-// class LoginScreen extends StatefulWidget {
-//   @override
-//   _LoginScreenState createState() => _LoginScreenState();
-// }
-
-// class _LoginScreenState extends State<LoginScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: Container(
-//           margin: EdgeInsets.all(50),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceAround,
-//             children: <Widget>[
-//               Padding(padding: EdgeInsets.all(50),
-//               child:
-//               Container(
-//                 width: 100.0,
-//                 height: 100.0,
-//                 decoration: BoxDecoration(
-//                   image: DecorationImage(
-//                       fit: BoxFit.cover, image: AssetImage('assets/icon/icon.png')),
-//                   borderRadius: BorderRadius.all(Radius.circular(15.0)),
-//                   color: Colors.black,
-//                 ),
-//               ),
-//               ),
-
-//               Text(
-//                 "LOGIN",
-//                 style: TextStyle(
-//                     fontFamily: 'fredoka',
-//                     color: Colors.black,
-//                     fontSize: 24,
-//                     fontWeight: FontWeight.bold),
-//               ),
-
-//               Padding(
-//                 padding: EdgeInsets.all(10),
-//                 child: TextField(
-//                   obscureText: true,
-//                   decoration: InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     labelText: 'Email',
-//                     icon: Icon(Icons.email)
-//                   ),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: EdgeInsets.all(10),
-//                 child: TextField(
-//                   obscureText: true,
-//                   decoration: InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     labelText: 'Password',
-//                     icon: Icon(Icons.password)
-
-//                   ),
-//                 ),
-//               ),
-
-//               // ElevatedButton(onPressed: null, child: Text("LOGIN", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),)
-//               // ),
-
-//               ElevatedButton.icon(onPressed: null,
-//                 icon: Icon(Icons.email), label:Text(
-//                 "SUBMIT",
-//                 style: TextStyle(
-//                     color: Colors.black, fontWeight: FontWeight.bold),
-//               ), ),
-
-//               ElevatedButton.icon(onPressed: null,
-//                 icon: Icon(Icons.phone_android), label: Text(
-//                 "LOGIN WITH OTP",
-//                 style: TextStyle(
-//                     color: Colors.black, fontWeight: FontWeight.bold),
-//               ),),
-
-//                   ElevatedButton.icon(onPressed: null,
-//                       icon: Icon(Icons.facebook,
-//                       ),
-//                       label: Text(
-//                     "LOGIN WITH FACEBOOK",
-//                     style: TextStyle(
-//                         color: Colors.black, fontWeight: FontWeight.bold,
-//                     ),
-//                   ),
-//                   ),
-//               //     ElevatedButton(
-//               //         onPressed: null,
-//               //         child: Text(
-//               //           "LOGIN WITH FACEBOOK",
-//               //           style: TextStyle(
-//               //               color: Colors.black, fontWeight: FontWeight.bold),
-//               //         )
-//               // ),
-
-//               Padding(
-//                 padding: EdgeInsets.all(10),
-//                 child: Container(
-//                   margin: EdgeInsets.only(top: 120),
-//                   alignment: Alignment.bottomCenter,
-//                   child: Column(
-//                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                       crossAxisAlignment: CrossAxisAlignment.stretch,
-//                       children: <Widget>[
-//                         Center(
-//                           child: Text(
-//                             "Don't have an account?",
-//                             style: TextStyle(
-//                                 color: Colors.red,
-//                                 fontWeight: FontWeight.bold,
-//                                 fontSize: 15),
-//                           ),
-//                         ),
-//                         // ElevatedButton(
-//                         //   Padding(padding: EdgeInsets.all(10
-//                         //   ),
-//                         //   child: Container(margin: EdgeInsets.only(top: 120),
-//                         //   alignment: Alignment.bottomCenter,
-//                         //   child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                         //   crossAxisAlignment: CrossAxisAlignment.stretch,),),
-//                         //   )
-//                         //   onPressed: null,
-//                         //   child: Text(
-//                         //     "Register",
-//                         //     style: TextStyle(
-//                         //         color: Colors.black,
-//                         //         fontWeight: FontWeight.w500,
-//                         //         fontSize: 30),
-//                         //   ),
-//                         // ),
-//                       ]),
-//                 ),
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -155,69 +9,147 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  late String _email;
-  late String _password;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+              Padding(
+                padding: EdgeInsets.all(50),
+                child: Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage('assets/icon/icon.png')),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.black,
+                  ),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _email = value!;
-                },
               ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+
+              Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontFamily: 'fredoka',
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      icon: Icon(Icons.email)),
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _password = value!;
-                },
               ),
-              SizedBox(height: 16.0),
-              RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // TODO: Implement login logic with _email and _password
-                  }
-                },
-                child: Text('Login'),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      icon: Icon(Icons.password)),
+                ),
               ),
+
+              // ElevatedButton(onPressed: null, child: Text("LOGIN", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),)
+              // ),
+
+              ElevatedButton.icon(
+                onPressed: null,
+                icon: Icon(Icons.email),
+                label: Text(
+                  "SUBMIT",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              ElevatedButton.icon(
+                onPressed: null,
+                icon: Icon(Icons.phone_android),
+                label: Text(
+                  "LOGIN WITH OTP",
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              ElevatedButton.icon(
+                onPressed: null,
+                icon: Icon(
+                  Icons.facebook,
+                ),
+                label: Text(
+                  "LOGIN WITH FACEBOOK",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              //     ElevatedButton(
+              //         onPressed: null,
+              //         child: Text(
+              //           "LOGIN WITH FACEBOOK",
+              //           style: TextStyle(
+              //               color: Colors.black, fontWeight: FontWeight.bold),
+              //         )
+              // ),
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Container(
+                  margin: EdgeInsets.only(top: 120),
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Center(
+                          child: Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15),
+                          ),
+                        ),
+                        // ElevatedButton(
+                        //   Padding(padding: EdgeInsets.all(10
+                        //   ),
+                        //   child: Container(margin: EdgeInsets.only(top: 120),
+                        //   alignment: Alignment.bottomCenter,
+                        //   child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        //   crossAxisAlignment: CrossAxisAlignment.stretch,),),
+                        //   )
+                        //   onPressed: null,
+                        //   child: Text(
+                        //     "Register",
+                        //     style: TextStyle(
+                        //         color: Colors.black,
+                        //         fontWeight: FontWeight.w500,
+                        //         fontSize: 30),
+                        //   ),
+                        // ),
+                      ]),
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
-
-  RaisedButton({required Null Function() onPressed, required Text child}) {}
 }
