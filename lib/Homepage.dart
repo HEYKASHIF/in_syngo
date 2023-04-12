16cc306b959d1e72a86c4ca04939901c9676bdc7
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-13 00:32:03 +0530

    13-april commit9

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -7,6 +7,7 @@ import 'package:in_syngo/fAQ.dart';
 import 'package:in_syngo/logOut.dart';
 import 'package:in_syngo/profile.dart';
 import 'package:in_syngo/sample.dart';
+import 'package:in_syngo/terms_&_Conditions.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -115,12 +116,12 @@ class HomePageState extends State<HomePage> {
             ),
             ListTile(
               leading: Icon(Icons.home),
-              title: Text("HOME"),
+              title: Text("TERMS & CONDITIONS"),
               onTap: () {
                 Navigator.of(context).pop();
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
+                  MaterialPageRoute(builder: (context) => TermsConditions()),
                 );
               },
             ),
--- lib/terms_&_Conditions.dart
+++ lib/terms_&_Conditions.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class TermsConditions extends StatefulWidget {
+  const TermsConditions({super.key});
+
+  @override
+  State<TermsConditions> createState() => _TermsConditionsState();
+}
+
+class _TermsConditionsState extends State<TermsConditions> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TERMS & CONDITIONS'),
+      ),
+    );
+  }
+}
