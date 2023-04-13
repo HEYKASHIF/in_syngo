3bd87661f751afbdd49445a0dc1ea8ef398a4e9d
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-14 05:14:45 +0530

    14-april commit11

--- lib/medicines.dart
+++ lib/medicines.dart
@@ -15,7 +15,7 @@ class medicineState extends State<medicines> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.blue,
         title: const Text("MEDICINES"),
       ),
       body: Column(
