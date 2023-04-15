376ee5f24f5b7b72e352256a0afe900f92a681d8
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-16 03:24:03 +0530

    16-april commit3

--- lib/medicines.dart
+++ lib/medicines.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:in_syngo/sample.dart';
 
 // ignore: camel_case_types
@@ -11,6 +15,29 @@ class medicines extends StatefulWidget {
 
 // ignore: camel_case_types
 class medicineState extends State<medicines> {
+  File? image;
+  Future pickImageGallery() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageCamera() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,14 +97,54 @@ class medicineState extends State<medicines> {
               ),
             ),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  child: Image.asset('assets/icon/medicine.png'),
+          Expanded(
+            child: Row(
+              children: [
+                (image != null)
+                    ? Expanded(
+                        child: Container(
+                          child: Image.file(
+                            image!,
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                      )
+                    : Expanded(
+                        child: Container(
+                          child: Image.asset(
+                            'assets/icon/icon.png',
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                      ),
+                Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageCamera,
+                          child: Text(
+                            "UPLOAD IMAGE BY CAMERA",
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageGallery,
+                          child: Text("UPLOAD IMAGE BY GALLERY"),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
