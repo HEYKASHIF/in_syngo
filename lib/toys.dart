5192b48e7d8dd734db49a824023e02766bfd4092
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-16 05:27:23 +0530

    16-april commit4

--- lib/toys.dart
+++ lib/toys.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 // ignore: camel_case_types
 class toys extends StatefulWidget {
@@ -10,6 +14,29 @@ class toys extends StatefulWidget {
 
 // ignore: camel_case_types
 class toysState extends State<toys> {
+  File? image;
+  Future pickImageGallery() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageCamera() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,59 +52,71 @@ class toysState extends State<toys> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Your Name Please',
-              ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Your Name Please',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Name of the Item',
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Name of the Item',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Weight of the Item',
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Weight of the Item',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Cost of Item Approximately',
-              ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Cost of Item Approximately',
             ),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  child: Image.asset('assets/icon/toy.png'),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+        ),
+        Expanded(
+            child: Row(
+          children: [
+            (image != null)
+                ? Expanded(
+                    child: Container(
+                      child: Image.file(
+                        image!,
+                        height: 100,
+                        width: 100,
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: Container(
+                    child: Image.asset(
+                      'assets/icon/icon.png',
+                      height: 100,
+                      width: 100,
+                    ),
+                  ))
+          ],
+        ))
+      ]),
     );
   }
 }
