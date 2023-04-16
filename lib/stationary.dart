8bcb20b59861310522a70d5a37db55013294f90f
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-16 05:43:30 +0530

    16-april commit6

--- lib/stationary.dart
+++ lib/stationary.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 // ignore: camel_case_types
 class stationary extends StatefulWidget {
@@ -10,6 +14,29 @@ class stationary extends StatefulWidget {
 
 // ignore: camel_case_types
 class stationaryState extends State<stationary> {
+  File? image;
+  Future pickImageGallery() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageCamera() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
