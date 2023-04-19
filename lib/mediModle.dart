092cdf3b0bf7c4e76cf717172e5ae2e43e1ec9e9
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 06:39:47 +0530

    19-april commit8

---
+++ lib/mediModle.dart
@@ -0,0 +1,10 @@
+class mediModle{
+  final String name;
+  final String mediName;
+  final String date;
+  final int no_of_tablets;
+  final String image;
+
+  mediModle(
+      this.name, this.mediName, this.date, this.no_of_tablets, this.image);
+}
