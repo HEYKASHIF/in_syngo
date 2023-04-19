a3b4e7a5c1ce9d3e492d503f61ce93d0b69e5c35
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 07:27:22 +0530

    19-april commit18

--- lib/profile.dart
+++ lib/profile.dart
@@ -16,6 +16,69 @@ class profileState extends State<profile> {
       appBar: AppBar(
         title: const Text("PROFILE"),
       ),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/prof.jpg'),
+                radius: 75,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 250, 10),
+            child: Text(
+              'MOHD KASHIF',
+              style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 275, 10),
+            child: Text(
+              'E-MAIL ID',
+              style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 300, 10),
+            child: Text(
+              'GENDER',
+              style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 225, 10),
+            child: Text(
+              'PHONE NUMBER',
+              style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 350, 10),
+            child: Text(
+              'AGE',
+              style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
