483efd63f7c3e84f8285f7835a95aca858feb8ec
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 07:09:02 +0530

    19-april commit16

--- lib/electronics.dart
+++ lib/electronics.dart
@@ -95,53 +95,61 @@ class electronicsState extends State<electronics> {
             ),
           ),
           Expanded(
-              child: Row(
-            children: [
-              (image != null)
-                  ? Expanded(
-                      child: Container(
-                        child: Image.file(
-                          image!,
-                          height: 100,
-                          width: 100,
+            child: Row(
+              children: [
+                (image != null)
+                    ? Expanded(
+                        child: Container(
+                          child: Image.file(
+                            image!,
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                      )
+                    : Expanded(
+                        child: Container(
+                          child: Image.asset(
+                            'assets/icon/icon.png',
+                            height: 100,
+                            width: 100,
+                          ),
                         ),
                       ),
-                    )
-                  : Expanded(
-                      child: Container(
-                        child: Image.asset(
-                          'assets/icon/icon.png',
-                          height: 100,
-                          width: 100,
+                Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageCamera,
+                          child: Text(
+                            'UPLOAD IMAGE BY CAMERA',
+                          ),
                         ),
                       ),
                     ),
-              Column(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
-                      child: ElevatedButton(
-                        onPressed: pickImageCamera,
-                        child: Text(
-                          'UPLOAD IMAGE BY CAMERA',
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageGallery,
+                          child: Text('UPLOAD IMAGE BY GALLERY'),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
-                      child: ElevatedButton(
-                        onPressed: pickImageGallery,
-                        child: Text('UPLOAD IMAGE BY GALLERY'),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ))
+                  ],
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(100, 5, 100, 5),
+            child: ElevatedButton(
+              onPressed: null,
+              child: Text('SUBMIT'),
+            ),
+          ),
         ],
       ),
     );
