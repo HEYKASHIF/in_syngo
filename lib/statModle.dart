cabcf1e02769ca9c3299b98645a438f57eec2933
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 06:42:50 +0530

    19-april commit9

---
+++ lib/statModle.dart
@@ -0,0 +1,9 @@
+class statModle{
+  final String name;
+  final String statName;
+  final double weight;
+  final int cost;
+  final String image;
+
+  statModle(this.name, this.statName, this.weight, this.cost, this.image);
+}
