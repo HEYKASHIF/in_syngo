5ee1738a656a094432c61d8fcb828d9f3fae3e39
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 06:36:30 +0530

    19-april commit7

---
+++ lib/foodModle.dart
@@ -0,0 +1,9 @@
+class foodModle{
+  final String name;
+  final String FoodName;
+  final double weight;
+  final int servings;
+  final String image;
+
+  foodModle(this.name, this.FoodName, this.weight, this.servings, this.image);
+}
