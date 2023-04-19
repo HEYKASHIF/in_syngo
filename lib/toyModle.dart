deed562a7b724b2a12a0ec77714579939b219fcf
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 06:46:20 +0530

    19-april commit10

---
+++ lib/toyModle.dart
@@ -0,0 +1,9 @@
+class toyModle{
+  final String name;
+  final String ItemName;
+  final double weight;
+  final int cost;
+  final String image;
+
+  toyModle(this.name, this.ItemName, this.weight, this.cost, this.image);
+}
