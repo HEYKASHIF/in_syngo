4167f48223fc84115e4ffa89266e65a56853dda8
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-19 06:32:01 +0530

    19-april commit6

---
+++ lib/electroModle.dart
@@ -0,0 +1,9 @@
+class electroModle{
+  final String name;
+  final String ItemName;
+  final double age;
+  final int cost;
+  final String image;
+
+  electroModle(this.name, this.ItemName, this.age, this.cost, this.image);
+}
