9c6fedc3227349f3ee714aa13ff6e266bc09c687
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-20 06:03:11 +0530

    20-april commit2

--- lib/contact_Us.dart
+++ lib/contact_Us.dart
@@ -14,6 +14,33 @@ class _ContactUSState extends State<ContactUS> {
       appBar: AppBar(
         title: Text('CONTACT US'),
       ),
+      body: Container(
+        // width: screenWidth,
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Contact Us:',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(height: 8),
+            ListTile(
+              leading: Icon(Icons.location_on),
+              title: Text('123 Main Street'),
+              subtitle: Text('Anytown, USA 12345'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('(123) 456-7890'),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('info@company.com'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
