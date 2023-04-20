78df0287e397b733cbc8aa5d9fc24006a979d343
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-20 15:08:05 +0530

    20-april commit22

---
+++ lib/signModle.dart
@@ -0,0 +1,11 @@
+class signModle {
+  final String name;
+  final String gender;
+  final int number;
+  final int age;
+  final String email;
+  final String password;
+
+  signModle(
+      this.name, this.gender, this.number, this.age, this.email, this.password);
+}
