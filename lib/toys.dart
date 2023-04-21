923294c39bf4053837d876b76ee9406da36657e2
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-21 10:49:10 +0530

    21-april commit13

--- lib/toys.dart
+++ lib/toys.dart
@@ -91,113 +91,116 @@ class toysState extends State<toys> {
           ),
         ],
       ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextFormField(
-            controller: nameController,
-            // obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Your Name Please',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: nameController,
+              // obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Your Name Please',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextFormField(
-            controller: ItemNameController,
-            // obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Name of the Item',
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: ItemNameController,
+              // obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name of the Item',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextFormField(
-            controller: weightController,
-            // obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Weight of the Item',
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: weightController,
+              // obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Weight of the Item',
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextFormField(
-            controller: costController,
-            // obscureText: true,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Cost of Item Approximately',
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextFormField(
+              controller: costController,
+              // obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Cost of Item Approximately',
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: Row(
-            children: [
-              (image != null)
-                  ? Expanded(
-                      child: Container(
-                        child: Image.file(
-                          image!,
-                          height: 100,
-                          width: 100,
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      (image != null)
+                          ? Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 75, 8, 8),
+                              child: Container(
+                                child: Image.file(
+                                  image!,
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ),
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 75, 8, 8),
+                              child: Container(
+                                child: Image.asset(
+                                  'assets/icon/icon.png',
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ),
+                            ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: sendData,
+                          child: Text('SUBMIT'),
                         ),
                       ),
-                    )
-                  : Expanded(
-                      child: Container(
-                        child: Image.asset(
-                          'assets/icon/icon.png',
-                          height: 100,
-                          width: 100,
-                        ),
-                      ),
-                    ),
-              Column(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
-                      child: ElevatedButton(
-                        onPressed: pickImageCamera,
-                        child: Text(
-                          'UPLOAD IMAGE BY CAMERA',
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(
-                        20,
-                        70,
-                        20,
-                        70,
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageCamera,
+                          child: Text(
+                            "UPLOAD IMAGE BY CAMERA",
+                          ),
+                        ),
                       ),
-                      child: ElevatedButton(
-                        onPressed: pickImageGallery,
-                        child: Text('UPLOAD IMAGE BY GALLERY'),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: pickImageGallery,
+                          child: Text("UPLOAD IMAGE BY GALLERY"),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              )
-            ],
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(100, 5, 100, 5),
-          child: ElevatedButton(
-            onPressed: sendData,
-            child: Text('SUBMIT'),
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
