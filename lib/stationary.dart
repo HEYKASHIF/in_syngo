aded87153800da850989588e409022741b55923a
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-21 10:50:09 +0530

    21-april commit14

--- lib/stationary.dart
+++ lib/stationary.dart
@@ -140,54 +140,63 @@ class stationaryState extends State<stationary> {
           Expanded(
             child: Row(
               children: [
-                (image != null)
-                    ? Expanded(
-                        child: Container(
-                        child: Image.file(
-                          image!,
-                          height: 100,
-                          width: 100,
-                        ),
-                      ))
-                    : Expanded(
-                        child: Container(
-                          child: Image.asset('assets/icon/icon.png'),
-                          height: 100,
-                          width: 100,
+                Expanded(
+                  child: Column(
+                    children: [
+                      (image != null)
+                          ? Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 75, 8, 8),
+                              child: Container(
+                                child: Image.file(
+                                  image!,
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ),
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 75, 8, 8),
+                              child: Container(
+                                child: Image.asset(
+                                  'assets/icon/icon.png',
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ),
+                            ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                        child: ElevatedButton(
+                          onPressed: sendData,
+                          child: Text('SUBMIT'),
                         ),
                       ),
-                Column(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
                         child: ElevatedButton(
                           onPressed: pickImageCamera,
                           child: Text(
-                            'UPLOAD IMAGE BY CAMERA',
+                            "UPLOAD IMAGE BY CAMERA",
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(20, 70, 20, 70),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 70, 20, 70),
                         child: ElevatedButton(
                           onPressed: pickImageGallery,
-                          child: Text('UPLOAD IMAGE BY GALLERY'),
+                          child: Text("UPLOAD IMAGE BY GALLERY"),
                         ),
                       ),
-                    ),
-                  ],
-                )
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(100, 5, 100, 5),
-            child: ElevatedButton(
-              onPressed: sendData,
-              child: Text('SUBMIT'),
             ),
           ),
         ],
