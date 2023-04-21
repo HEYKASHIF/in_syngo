15154d562595571077713b58323ad995635fdbf3
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-21 09:27:41 +0530

    21-april commit2

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,8 @@
 //import 'dart:ffi';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:in_syngo/Homepage.dart';
 import 'package:in_syngo/Signup_Screen.dart';
