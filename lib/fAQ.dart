565c1b680fc31eef9b36a82587668bdfb6423ab3
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-25 23:28:53 +0530

    25-april commit2

--- lib/fAQ.dart
+++ lib/fAQ.dart
@@ -15,7 +15,7 @@ class _FAQState extends State<FAQ> {
           'Flutter is a mobile app development framework created by Google. It allows developers to build natively compiled applications for mobile, web, and desktop from a single codebase.'
     },
     {
-      'question': 'What are the benefits of using Flutter?',
+      'question': 'What are the benefits of using IN_SYNGO?',
       'answer':
           'There are several benefits of using Flutter, including: fast development, expressive and flexible UI, hot reload, and good performance.'
     },
